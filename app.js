3a69deacfa333b573d510cc05e28dd3534eb8093
Sebastian Kreuß <sebastian.kreuss@coconet.de>
2024-12-31 08:05:41 +0100

    start button

--- app.js
+++ app.js
@@ -2,34 +2,75 @@ let vocab = [];
 let currentIndex = 0;
 let showingAnswer = false;
 
-document.getElementById('fileInput').addEventListener('change', function(event) {
+function nextQuestionOrAnswer() {
+    if (vocab.length === 0) return;
+
+    if (currentIndex >= vocab.length) {
+        hideCard();
+        showUploadForm();
+    }
+
+    if (showingAnswer) {
+        currentIndex++;
+        showQuestion();
+    } else {
+        showAnswer();
+    }
+}
+
+function uploadJson(event) {
     const file = event.target.files[0];
     if (file) {
         const reader = new FileReader();
         reader.onload = function(e) {
             try {
                 vocab = JSON.parse(e.target.result);
-                vocab = shuffleArray(vocab);
                 currentIndex = 0;
-                showQuestion();
             } catch (error) {
                 alert('Invalid JSON file');
             }
         };
         reader.readAsText(file);
     }
-});
+}
 
-document.getElementById('card').addEventListener('click', function() {
-    if (vocab.length === 0) return;
+function showUploadForm() {
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.id = 'fileInput';
+    input.accept = 'application/json';
+    input.onchange = uploadJson;
 
-    if (showingAnswer) {
-        currentIndex = (currentIndex + 1) % vocab.length;
-        showQuestion();
-    } else {
-        showAnswer();
-    }
-});
+    document.body.appendChild(input);
+
+    const button = document.createElement('button');
+    button.innerText = 'Start';
+    button.id = 'startButton';
+    button.onclick = start;
+
+    document.body.appendChild(start);
+}
+
+function hideUploadForm() {
+    const input = document.getElementById('fileInput');
+    document.body.removeChild(input);
+
+    const button = document.getElementById('startButton');
+    document.body.removeChild(button);
+}
+
+function showCard() {
+    const card = document.createElement('div');
+    card.id = 'card';
+    card.onclick = nextQuestionOrAnswer;
+
+    document.body.appendChild(card);
+}
+
+function hideCard() {
+    const card = document.getElementById('card');
+    document.body.removeChild(card);
+}
 
 function showQuestion() {
     showingAnswer = false;
@@ -43,4 +84,15 @@ function showAnswer() {
 
 function shuffleArray(array) {
     return array.sort(() => Math.random() - 0.5);
+}
+
+function start() {
+    vocab = shuffleArray(vocab);
+    hideUploadForm();
+    showCard();
+    showQuestion();
+}
+
+window.onload = () => {
+    showUploadForm();
 }
